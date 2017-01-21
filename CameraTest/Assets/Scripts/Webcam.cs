8955cae8b628f128527232cf279a87d503389f61
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2017-01-21 20:23:16 +0000

    First version with real videos

---
+++ CameraTest/Assets/Scripts/Webcam.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Webcam : MonoBehaviour
+{
+
+	public MeshRenderer[] UseWebcamTexture;
+	private WebCamTexture webcamTexture;
+
+	void Start()
+	{
+		webcamTexture = new WebCamTexture();
+		foreach(MeshRenderer r in UseWebcamTexture)
+		{
+			r.material.mainTexture = webcamTexture;
+		}
+		GetComponent<Renderer>().material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+
+	void OnGUI()
+	{
+		if (webcamTexture.isPlaying)
+		{
+			if (GUILayout.Button("Pause"))
+			{
+				webcamTexture.Pause();
+			}
+			if (GUILayout.Button("Stop"))
+			{
+				webcamTexture.Stop();
+			}
+		}
+		else
+		{
+			if (GUILayout.Button("Play"))
+			{
+				webcamTexture.Play();
+			}
+		}
+	}
+}
