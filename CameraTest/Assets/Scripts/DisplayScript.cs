955c557964798060c292c961bd1bdda0d57f1711
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2017-05-26 18:53:39 +0100

    Newest version

---
+++ CameraTest/Assets/Scripts/DisplayScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayScript : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+		Debug.Log("displays connected: " + Display.displays.Length);
+		// Display.displays[0] is the primary, default display and is always ON.
+		// Check if additional displays are available and activate each.
+		if (Display.displays.Length > 2) {
+			Debug.Log ("Using multi-display support");
+			Display.displays [1].Activate ();
+			Display.displays [2].Activate ();
+		} else {
+			Debug.Log ("Activating split screen mode.");
+			Camera left = GameObject.Find ("Left Camera").GetComponent<Camera>();
+			Camera middle = GameObject.Find ("Main Camera").GetComponent<Camera>();
+			Camera right = GameObject.Find ("Right Camera").GetComponent<Camera>();
+			left.rect = new Rect (0.0f, 0.0f, 1.0f/3.0f, 1.0f);
+			middle.rect = new Rect (1.0f/3.0f, 0.0f, 1.0f/3.0f, 1.0f);
+			right.rect = new Rect (2.0f/3.0f, 0.0f, 1.0f/3.0f, 1.0f);
+			left.targetDisplay = 0;
+			middle.targetDisplay = 0;
+			right.targetDisplay = 0;
+		}
+	}
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ CameraTest/Assets/SpotMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotMove : MonoBehaviour {
+	Transform spottrans;
+	float angle = -120;
+
+	// Use this for initialization
+	void Start () {
+		spottrans = GameObject.Find ("Spotlight").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		angle += 1;
+		if (angle > 120)
+			angle = -120;
+		spottrans.eulerAngles = new Vector3 (0, angle, 0);
+	}
+}
